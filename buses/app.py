9a6512529a969bab1f44164cc19646a2371ccf49
Beau <beau@beaumartinez.com>
2013-05-05 16:14:20 +0100

    Add Flasky bits

---
+++ buses/app.py
@@ -0,0 +1,22 @@
+import flask
+
+import __init__ as buses
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return flask.render_template('home.html')
+
+
+@app.route('/arrival-times/')
+@app.route('/arrival-times/<latitude>/<longitude>/')
+@app.route('/arrival-times/<latitude>/<longitude>/<radius>/')
+def get_arrival_times(latitude=None, longitude=None, radius=100):
+    return buses.get_arrival_times(latitude, longitude, radius)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
