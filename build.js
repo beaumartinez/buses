d5d4a28796cdbfe416276e1244b3c114050bfb8a
Beau <beau@beaumartinez.com>
2013-05-06 13:37:03 +0100

    Concat CSS in build

--- build.js
+++ build.js
@@ -1,10 +1,20 @@
 ({
-    baseUrl: 'buses/static/js/',
+    baseUrl: 'buses/static/',
     dir: 'buses/build',
 
     modules: [{
         name: 'main',
     }],
+
+    paths: {
+        fastclick: 'js/fastclick',
+        ga: 'js/ga',
+        handlebars: 'js/handlebars',
+        jquery: 'js/jquery',
+        main: 'js/main',
+        moment: 'js/moment',
+        underscore: 'js/underscore',
+    },
 
     shim: {
         underscore: {
