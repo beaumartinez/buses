c2fbecda943140a11d812a4471e7e6169f91e488
Beau <beau@beaumartinez.com>
2013-05-11 00:26:38 +0100

    Split debug logic to another module

--- buses/app.py
+++ buses/app.py
@@ -11,7 +11,3 @@ def get_arrival_times(latitude, longitude):
     arrival_times = buses.get_arrival_times(latitude, longitude)
 
     return flask.Response(arrival_times, mimetype='application/json')
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
