594146038fcd13d3f29c7f600b2660859fc45122
Beau <beau@beaumartinez.com>
2013-05-10 22:20:28 +0100

    Don't serve root with Flask
    We serve it with nginx directly

--- buses/app.py
+++ buses/app.py
@@ -6,15 +6,7 @@ import __init__ as buses
 app = flask.Flask(__name__)
 
 
-@app.route('/')
-def home():
-    with open('buses/templates/home.html', 'r') as file_:
-        content = file_.read()
-
-    return content
-
-
-@app.route('/arrival-times/<latitude>/<longitude>/')
+@app.route('/<latitude>/<longitude>/')
 def get_arrival_times(latitude, longitude):
     arrival_times = buses.get_arrival_times(latitude, longitude)
 
