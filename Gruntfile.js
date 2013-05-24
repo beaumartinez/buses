93cdb8937b1d0d45e590e71aa224cb938207d4e0
Beau <beau@beaumartinez.com>
2013-05-24 21:58:14 +0100

    Copy Bower packages to static
    Boom

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,40 @@ module.exports = function(grunt) {
     grunt.initConfig({
         bower: {
             install: {
-
+                cleanTargetDir: true
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true, 
+                        src: ['lib/**/*.js'],
+                        dest: 'www/static/js/',
+                        filter: 'isFile',
+                        rename: function(destination, source) {
+                            if (source.indexOf('hammer') !== -1) {
+                                return destination + 'hammer.js';
+                            } else {
+                                return destination + source; 
+                            }
+                        }
+                    },
+                    {
+                        expand: true,
+                        flatten: true, 
+                        src: ['lib/**/*.css'],
+                        dest: 'www/static/css/',
+                        filter: 'isFile'
+                    },
+                ]
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-bower-task');
 
-    grunt.registerTask('default', ['bower']);
+    grunt.registerTask('default', ['bower', 'copy']);
 };
