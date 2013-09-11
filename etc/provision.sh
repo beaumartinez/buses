dc557ccdfbc6e524fbd1023ba85eb641978c6aa6
Beau <beau@beaumartinez.com>
2013-09-11 19:06:23 +0200

    Start grunt-watch upstart task

--- etc/provision.sh
+++ etc/provision.sh
@@ -44,7 +44,11 @@ npm install -g bower grunt-cli
 # We're symlinking the Upstart job. Upstart doesn't work well with symlinks, so
 # we have to call "initctl reload-configuration" explicitly for it to recognize
 # it
+
 ln -s /vagrant/etc/buses.conf /etc/init/buses.conf
 ln -s /vagrant/etc/grunt-watch.conf /etc/init/grunt-watch.conf
+
 initctl reload-configuration
+
 service buses start
+service grunt-watch start
