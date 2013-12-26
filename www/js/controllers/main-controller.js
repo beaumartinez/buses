1af9dcbff1cc39bb6bd5c1818be798f51d5cd0ef
Beau <beau@beaumartinez.com>
2013-12-26 19:53:40 +0100

    Make AngularJS things work
    Woo!

--- www/js/controllers/main-controller.js
+++ www/js/controllers/main-controller.js
@@ -1,5 +1,74 @@
 'use strict';
 
-app.controller('mainController', ['$scope', function($scope) {
-    $scope.beau = 'Hai gais';
+app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+    navigator.geolocation.watchPosition(function(geoposition) {
+        $http.get('/arrival-times/' + geoposition.coords.latitude + '/' + geoposition.coords.longitude + '/').success(function(allArrivals) {
+            // allArrivals is an array of all arrivals, ungrouped by bus stop and unsorted.
+            // We need to group and sort them.
+
+            if (allArrivals.length === 0) {
+                $scope.error = "No bus arrival data. There might not be buses in your area. TFL services might also be down.";
+            } else {
+                // Group by bus stop
+
+                var stopLookup = _.groupBy(allArrivals, 'stopId');
+
+                // Sort by distance
+
+                var stopIds = Object.getOwnPropertyNames(stopLookup);
+
+                stopIds = stopIds.sort(function(a, b) {
+                    var aExample = stopLookup[a][0];
+                    var bExample = stopLookup[b][0];
+
+                    return aExample.distance - bExample.distance;
+                });
+
+                // Set $scope.stops
+
+                $scope.stops = [];
+
+                stopIds.forEach(function(stopId) {
+                    var stopArrivals = stopLookup[stopId];
+
+                    // Sort by ETA
+                    stopArrivals = _.sortBy(stopArrivals, 'eta');
+
+                    // Format ETAs nicely
+                    stopArrivals.map(function(stopArrival) {
+                        var eta = moment(stopArrival.eta);
+                        eta = eta.fromNow(true);
+
+                        stopArrival.eta = eta;
+
+                        return stopArrival;
+                    });
+
+                    // Format distances nicely
+                    stopArrivals.map(function(stopArrival) {
+                        stopArrival.distance = stopArrival.distance * 1000;
+                        stopArrival.distance = stopArrival.distance.toFixed(0);
+
+                        return stopArrival;
+                    });
+
+                    var exampleArrival = stopArrivals[0];
+
+                    $scope.stops.push({
+                        stopName: exampleArrival.stopName,
+                        stopCode: exampleArrival.stopCode,
+                        towards: exampleArrival.towards,
+                        distance: exampleArrival.distance,
+
+                        arrivals: stopArrivals,
+                    });
+                });
+            }
+
+            $scope.accuracy = geoposition.coords.accuracy.toFixed(0);
+        }).error(function(response) {
+            $scope.error = (response.status === 502) ? "TFL services are down. " : "Couldn't load bus arrival data. ";
+            $scope.error += "Please try again later.";
+        });
+    });
 }]);
--- www/js/main.js
+++ www/js/main.js
@@ -2,8 +2,6 @@
     'use strict';
 
     $(function() {
-        navigator.geolocation.watchPosition(doShit, doShitError);
-
         window.setTimeout(function() {
             var spinner = $('.spinner');
             spinner.removeClass('hidden');
@@ -41,76 +39,6 @@
         var accuracy = document.getElementById('accuracy');
 
         var collapseAll = document.getElementById('collapse-all');
-
-        function doShit(geoposition) {
-            loading.innerHTML = "Loading bus arrival data...";
-
-            $.ajax('/arrival-times/' + geoposition.coords.latitude + '/' + geoposition.coords.longitude + '/').done(function(responseArrivals) {
-                if (responseArrivals.length === 0) {
-                    _error("No bus arrival data.<p>There might not be buses in your area. TFL services might also be down.");
-                } else {
-                    $(content).empty();
-
-                    var stopLookup = _.groupBy(responseArrivals, 'stopId');
-
-                    var stopIds = Object.getOwnPropertyNames(stopLookup);
-                    stopIds = stopIds.sort(function(a, b) {
-                        var aExample = stopLookup[a][0];
-                        var bExample = stopLookup[b][0];
-
-                        return aExample.distance - bExample.distance;
-                    });
-
-                    loading.parentNode.classList.add('hidden');
-
-                    stopIds.forEach(function(stopId) {
-                        var arrivals = stopLookup[stopId];
-                        arrivals = _.sortBy(arrivals, 'eta');
-
-                        // Timeago ETAs
-                        arrivals.map(function(arrival) {
-                            var eta = moment(arrival.eta);
-                            eta = eta.fromNow(true);
-
-                            arrival.eta = eta;
-
-                            return arrival;
-                        });
-                        arrivals.map(function(arrival) {
-                            arrival.distance = arrival.distance * 1000;
-                            arrival.distance = arrival.distance.toFixed(0);
-
-                            return arrival;
-                        });
-
-                        var exampleArrival = arrivals[0];
-
-                        var renderedTemplate = stopTemplate({
-                            exampleArrival: exampleArrival,
-                            arrivals: arrivals,
-                        });
-
-                        // Add template, hide loading, add attribution
-
-                        content.innerHTML += renderedTemplate;
-                    });
-
-                    collapseAll.classList.remove('hidden');
-                }
-
-                accuracy.innerHTML = geoposition.coords.accuracy.toFixed(0);
-                accuracy.parentNode.classList.remove('hidden');
-            }).fail(function(response) {
-                var errorMessage = (response.status === 502) ? "TFL services are down." : "Couldn't load bus arrival data.";
-                errorMessage += " " + "Please try again later.";
-
-                _error(errorMessage);
-            });
-        }
-        
-        function doShitError(geopositionError) {
-            _error("Couldn't get location data." + " (" + geopositionError.message + ").");
-        }
 
         $('body').on('click', '.stop', function(event) {
             event.preventDefault();
