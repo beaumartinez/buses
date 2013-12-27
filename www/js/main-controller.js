ebf897772d69e5a17e8ced3f3d204f8c6f22b5a9
Beau <beau@beaumartinez.com>
2013-12-27 16:45:08 +0100

    Better stop controller logic

--- www/js/main-controller.js
+++ www/js/main-controller.js
@@ -1,21 +1,27 @@
 (function() {
     'use strict';
 
-    window.app.controller('mainController', ['$scope', '$http', function($scope, $http) {
+    window.app.controller('mainController', ['$scope', '$http', '$filter', function($scope, $http, $filter) {
+        var unique = $filter('unique');
+        var orderBy = $filter('orderBy');
+
         $scope.loading = 'Getting your location...';
 
         navigator.geolocation.watchPosition(function(geoposition) {
             var url = '/arrival-times/' + geoposition.coords.latitude + '/' + geoposition.coords.longitude + '/';
 
+            $scope.error = undefined;
             $scope.loading = 'Getting bus stops...';
+
             $http.get(url).success(function(allArrivals) {
                 $scope.loading = false;
-
-                $scope.allArrivals = allArrivals;
                 $scope.accuracy = geoposition.coords.accuracy;
 
                 if (allArrivals.length === 0) {
                     $scope.error = "No bus stops. There might not be buses in your area. TFL services might also be down.";
+                } else {
+                    $scope.allArrivals = allArrivals;
+                    $scope.stops = orderBy(unique(allArrivals, 'stopId'), 'distance');
                 }
             }).error(function(response) {
                 $scope.error = (response.status === 502) ? "TFL " : "Our ";
