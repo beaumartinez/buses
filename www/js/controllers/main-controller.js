1c89e89572ce5f4cee15f2963e4ba7e90a10d713
Beau <beau@beaumartinez.com>
2013-12-27 12:44:41 +0100

    Copy tweaks

--- www/js/controllers/main-controller.js
+++ www/js/controllers/main-controller.js
@@ -5,20 +5,21 @@
         $scope.loading = 'Getting your location...';
 
         navigator.geolocation.watchPosition(function(geoposition) {
-            $scope.loading = 'Getting arrivals...';
+            var url = '/arrival-times/' + geoposition.coords.latitude + '/' + geoposition.coords.longitude + '/';
 
-            $http.get('/arrival-times/' + geoposition.coords.latitude + '/' + geoposition.coords.longitude + '/').success(function(allArrivals) {
+            $scope.loading = 'Getting bus stops...';
+            $http.get(url).success(function(allArrivals) {
                 $scope.loading = false;
 
                 $scope.allArrivals = allArrivals;
                 $scope.accuracy = geoposition.coords.accuracy;
 
                 if (allArrivals.length === 0) {
-                    $scope.error = "No bus arrival data. There might not be buses in your area. TFL services might also be down.";
+                    $scope.error = "No bus stops. There might not be buses in your area. TFL services might also be down.";
                 }
             }).error(function(response) {
-                $scope.error = (response.status === 502) ? "TFL services are down. " : "Couldn't load bus arrival data. ";
-                $scope.error += "Please try again later.";
+                $scope.error = (response.status === 502) ? "TFL " : "Our ";
+                $scope.error += "services are down. Please try again later.";
             });
         });
     }]);
