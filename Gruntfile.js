51c7190cf687470e8687d2c8a1b2639e4c35daa6
Beau <beau@beaumartinez.com>
2013-12-30 11:23:25 +0100

    No more concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,36 +90,20 @@ module.exports = function(grunt) {
             },
         },
 
-        concat: {
-            js: {
-                // Include main.js and ga.js at the end
-                // Include controllers just before them
-                src: [
-                    'prebuild/*.js', 
-                    '!prebuild/*-controller.js', 
-                    'prebuild/*-controller.js', 
-                    '!prebuild/main.js',
-                    'prebuild/main.js',
-                    '!prebuild/ga.js',
-                    'prebuild/ga.js',
-                ],
-                dest: 'prebuild/main.js',
-            },
-            css: {
-                // Include style.css at the end
-                src: [
-                    'prebuild/*.css',
-                    '!prebuild/style.css',
-                    'prebuild/style.css',
-                ],
-                dest: 'prebuild/style.css',
-            },
-        },
-
         uglify: {
             js: {
                 files: {
-                    'build/main.js': ['prebuild/main.js'],
+                    // Include main.js and ga.js at the end
+                    // Include controllers just before them
+                    'build/main.js': [
+                        'prebuild/*.js', 
+                        '!prebuild/*-controller.js', 
+                        'prebuild/*-controller.js', 
+                        '!prebuild/main.js',
+                        'prebuild/main.js',
+                        '!prebuild/ga.js',
+                        'prebuild/ga.js',
+                    ],
                 },
             },
         },
@@ -130,7 +114,12 @@ module.exports = function(grunt) {
                     keepSpecialComments: 0,
                 },
                 files: {
-                    'build/style.css': ['prebuild/style.css'],
+                    // Include style.css at the end
+                    'build/style.css': [
+                        'prebuild/*.css',
+                        '!prebuild/style.css',
+                        'prebuild/style.css',
+                    ],
                 },
             },
         },
@@ -164,9 +153,9 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('watch-css', ['clean:css', 'copy:bowerCss', 'copy:css', 'concat:css', 'cssmin']);
-    grunt.registerTask('watch-js', ['clean:js', 'copy:bowerJs', 'copy:js', 'concat:js', 'uglify']);
+    grunt.registerTask('watch-css', ['clean:css', 'copy:bowerCss', 'copy:css', 'cssmin']);
+    grunt.registerTask('watch-js', ['clean:js', 'copy:bowerJs', 'copy:js', 'uglify']);
     grunt.registerTask('watch-html', ['clean:html', 'htmlmin']);
 
-    grunt.registerTask('default', ['clean:all', 'bower', 'copy', 'concat', 'htmlmin', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['clean:all', 'bower', 'copy', 'htmlmin', 'cssmin', 'uglify']);
 };
